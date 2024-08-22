7c67cb58ff97730e906f5ce34bf9d6668d2bf353
cedricrenouleau <ced241@gmail.com>
2024-08-22 15:16:32 +0200

    commit gitignore

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="movies-list">
+    <div class="movie" v-for="(movie, index) in movies" :key="movie.id">
+      <img :src="movie.image" :alt="movie.name">
+      <div class="movie-description">
+        <h1>{{movie.name}}</h1>
+
+        <div class="genres">
+          <span v-for="(genre, index) in movie.genres" class="genre" :key="index">{{ genre }}</span>
+        </div>
+
+        <p>{{movie.description}}</p>
+        <div class="rating">
+          <p>Rating: ({{ movie.rating }}/5) </p>
+
+            <button class="star-button" @click.once="rateFilm(note, index)" v-for="note in 5">
+              <StarIcon v-if="note <= movie.rating" style="color: #FFD250" />
+              <StarIcon v-else style="color: grey" />
+            </button>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { items } from "./movies.json";
+import { StarIcon } from "@heroicons/vue/24/solid";
+import {ref} from 'vue'
+
+const movies = ref(items)
+
+function rateFilm(note, index) {
+  movies.value[index].rating = note
+}
+
+</script>
+
+
+<style scoped>
+
+.movies-list {
+  display: grid; 
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  margin: 5rem 0;
+}
+
+.movie {
+  border-radius: 15px;
+  overflow: auto;
+  background-color: #FFF;
+  width: 400px;
+}
+
+.movie img {
+  object-fit: cover;
+  width: 100%;
+  height: 500px;
+}
+
+.movie-description {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h1 {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.genres {
+  display: flex;
+  flex-direction: row;
+}
+
+.genre {
+  color: #fff;
+  background-color: #6466F1;
+  border-radius: 10rem;
+  width: fit-content;
+  padding: .2rem .5rem;
+  margin: 0 .2rem;
+}
+
+.rating {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.star-button {
+  width: 32px;
+  height: 32px;
+}
+</style>
